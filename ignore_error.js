5ea1f4e1a1f8d6453fe6e6d063438a3fff67ecc7
Alexander Gugel <alexander.gugel@gmail.com>
2015-08-25 23:14:24 +0200

    Document ignore_error.js

--- ignore_error.js
+++ ignore_error.js
@@ -1,8 +1,17 @@
 'use strict'
 
-function ignoreError (ignore, cb) {
+/**
+ * Utility function used for creating wrapper functions used for filtering out
+ * errors.
+ *
+ * @param  {String}   code  The code of the error to be ignored.
+ * @param  {Function} cb    The callback function to be wrapped.
+ * @return {Function}       A wrapper function that filters out errors with the
+ *                          predefined code.
+ */
+function ignoreError (code, cb) {
   return function (err) {
-    var args = [!err || err.code === ignore ? null : err].concat(
+    var args = [!err || err.code === code ? null : err].concat(
       Array.prototype.slice.call(arguments, 1)
     )
     cb.apply(null, args)
