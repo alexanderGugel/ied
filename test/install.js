6df0487d02f00cb3c90f2175c2dc4ef419148826
Alexander Gugel <alexander.gugel@gmail.com>
2015-10-24 19:50:06 +0100

    Add more tests

---
+++ test/install.js
@@ -0,0 +1,39 @@
+/* global describe it beforeEach */
+
+var proxyquire = require('proxyquire')
+var assert = require('assert')
+var mock = require('mockmock')
+
+describe('install', function () {
+  var forceSymlink
+  var resolve
+  var download
+  var ignoreError
+  var install
+  var cb
+  var log
+
+  beforeEach(function () {
+    forceSymlink = mock()
+    resolve = mock()
+    download = mock()
+    ignoreError = mock()
+    cb = mock()
+    log = {
+      info: mock()
+    }
+
+    install = proxyquire('../lib/install', {
+      './force_symlink': forceSymlink,
+      './resolve': resolve,
+      './download': download,
+      './ignore_error': ignoreError,
+      'a-logger': log
+    })
+  })
+
+  it('should resolve correct package', function () {
+    install('/somewhere/', 'name', '~1.0.0', false, cb)
+    assert.deepEqual(resolve.args[0].slice(0, 2), ['name', '~1.0.0'])
+  })
+})
