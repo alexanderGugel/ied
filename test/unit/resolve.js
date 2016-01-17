7cedf86e7c82c85ae999fb52c65681c820ab93a8
just-boris <just-boris@hotmail.com>
2016-01-17 22:34:57 +0300

    use got instead of raw http

--- test/unit/resolve.js
+++ test/unit/resolve.js
@@ -1,189 +1,165 @@
-/* global describe it beforeEach */
+/* global describe it before after beforeEach afterEach */
 
 var assign = require('object-assign')
 var proxyquire = require('proxyquire')
 var assert = require('assert')
-var mock = require('mockmock')
-var EventEmitter = require('events').EventEmitter
+var nock = require('nock')
 var config = require('../../lib/config')
 
 describe('resolve', function () {
+  before(function () {
+    nock.disableNetConnect()
+  })
+  after(function () {
+    nock.enableNetConnect()
+  })
+
   describe('npm-registry', function () {
     var protocols = ['http', 'https']
     protocols.forEach(function (protocol) {
       describe(protocol, function () {
-        var http
-        var cb
-        var httpResp
-        var httpGet
+        var server
         var resolve
         beforeEach(function () {
-          httpResp = new EventEmitter()
-          httpGet = new EventEmitter()
-          http = {
-            get: mock(httpGet),
-            '@global': true
-          }
           var dummyConfig = assign({}, config)
           dummyConfig['@global'] = true
+          dummyConfig.requestRetries = 0
           dummyConfig.registry = protocol + '://registry.npmjs.org/'
-          cb = mock()
+          server = nock(dummyConfig.registry)
 
           resolve = proxyquire('../../lib/resolve', {
-            '../config': dummyConfig,
-            http: http,
-            https: http
+            '../config': dummyConfig
           })
         })
 
-        it('should fetch from correct endpoint', function () {
-          var name = 'some-package'
-          var version = '~1.0.0'
-          resolve(name, version, cb)
-
-          assert.deepEqual(http.get.args[0][0].href, protocol + '://registry.npmjs.org/' + name)
+        afterEach(function () {
+          server.done()
         })
 
         it('should accept optional callback', function () {
           var err = new Error()
+          server.get('/some-package').replyWithError(err)
           resolve('some-package', '~1.0.0')
+        })
 
-          assert.doesNotThrow(function () {
-            httpGet.emit('error', err)
+        it('should handle HTTP connection error', function (done) {
+          var err = new Error('Some request error')
+          server.get('/some-package').replyWithError(err)
+          resolve('some-package', '~1.0.0', function (requestError) {
+            assert.equal(requestError.message, err.message)
+            done()
           })
         })
 
-        it('should handle HTTP connection error', function () {
-          var err = new Error()
-          resolve('some-package', '~1.0.0', cb)
-          httpGet.emit('error', err)
-          assert.equal(cb.args[0][0], err)
+        it('should handle invalid statusCode', function (done) {
+          server.get('/some-package').reply(404, 'package not found')
+          resolve('some-package', '~1.0.0', function (err) {
+            assert(err)
+            done()
+          })
         })
 
-        it('should handle HTTP error while receiving data', function () {
-          var err = new Error()
-          resolve('some-package', '~1.0.0', cb)
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-          httpResp.emit('data', 'some data')
-          httpResp.emit('error', err)
-          assert.equal(cb.args[0][0], err)
+        it('should handle invalid JSON response', function (done) {
+          server.get('/some-package').reply(200, 'invalid payload')
+          resolve('some-package', '~1.0.0', function (err) {
+            assert.equal(err.name, 'ParseError')
+            done()
+          })
         })
 
-        it('should handle invalid statusCode', function () {
-          resolve('some-package', '~1.0.0', cb)
-          httpResp.statusCode = 404
-          http.get.args[0][1](httpResp)
-          httpResp.emit('data', '{}')
-          httpResp.emit('end')
-          assert(cb.args[0][0])
-        })
-
-        it('should handle invalid JSON response', function () {
-          resolve('some-package', '~1.0.0', cb)
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-          httpResp.emit('data', 'some data')
-          httpResp.emit('data', 'some more data')
-          httpResp.emit('end')
-          assert(cb.args[0][0] instanceof SyntaxError)
-        })
-
-        it('should validate JSON response schema to have plain object property "versions', function () {
-          resolve('some-package', '~1.0.0', cb)
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-          httpResp.emit('data', JSON.stringify({
+        it('should validate JSON response schema to have plain object property "versions', function (done) {
+          server.get('/some-package').reply(200, {
             versions: null
-          }))
-          httpResp.emit('end')
-          assert(cb.args[0][0] instanceof SyntaxError)
+          })
+          resolve('some-package', '~1.0.0', function (err) {
+            assert(err instanceof SyntaxError)
+            done()
+          })
         })
 
-        it('should handle case of no matching version', function () {
-          resolve('some-package', '~5.0.0', cb)
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-          httpResp.emit('data', JSON.stringify({
+        it('should handle case of no matching version', function (done) {
+          server.get('/some-package').reply(200, {
             versions: {
               '0.0.1': {},
               '1.0.0': {},
               '2.0.0': {}
             }
-          }))
-          httpResp.emit('end')
-          assert(cb.args[0][0] instanceof Error)
+          })
+          resolve('some-package', '~5.0.0', function (err) {
+            assert(err instanceof Error)
+            done()
+          })
         })
 
-        it('should resolve to latest correct version', function () {
-          resolve('some-package', '~1.1.0', cb)
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-          httpResp.emit('data', JSON.stringify({
+        it('should resolve to latest correct version', function (done) {
+          server.get('/some-package').reply(200, {
             versions: {
               '0.0.1': { version: '0.0.1' },
               '1.0.0': { version: '1.0.0' },
-              '1.1.0': { version: '1.1.0', dist: {} },
+              '1.1.0': { version: '1.1.0', name: 'some-package', dist: {} },
               '2.0.0': { version: '2.0.0' }
             }
-          }))
-          httpResp.emit('end')
-          assert.ifError(cb.args[0][0])
-          assert.deepEqual(cb.args[0][1].version, '1.1.0')
+          })
+          resolve('some-package', '~1.1.0', function (err, pkg) {
+            assert.ifError(err)
+            assert.deepEqual(pkg, {
+              name: 'some-package',
+              version: '1.1.0',
+              uid: 'f379f6f508a68b25ecea93893d69e7daa7f965bf',
+              shasum: undefined,
+              tarball: undefined
+            })
+            done()
+          })
         })
 
-        it('should properly handle pending requests', function () {
-          var cb2 = mock()
+        it('should properly handle pending requests', function (doneAll) {
+          var pendingRequests = 2
+          function done () {
+            pendingRequests--
+            if (!pendingRequests) {
+              doneAll()
+            }
+          }
 
-          resolve('some-package', '~1.1.0', cb)
-          resolve('some-package', '~2.0.0', cb2)
-
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-
-          httpResp.emit('data', JSON.stringify({
+          server.get('/some-package').reply(200, {
             versions: {
-              '0.0.1': { version: '0.0.1' },
-              '1.0.0': { version: '1.0.0' },
+              '0.0.1': { version: '0.0.1', dist: {} },
+              '1.0.0': { version: '1.0.0', dist: {} },
               '1.1.0': { version: '1.1.0', dist: {} },
               '2.0.0': { version: '2.0.0', dist: {} }
             }
-          }))
-          httpResp.emit('end')
+          })
 
-          assert.ifError(cb.args[0][0])
-          assert.ifError(cb2.args[0][0])
-          assert.deepEqual(cb.args[0][1].version, '1.1.0')
-          assert.deepEqual(cb2.args[0][1].version, '2.0.0')
+          resolve('some-package', '~1.1.0', function (err, pkg) {
+            assert.ifError(err)
+            assert.deepEqual(pkg.version, '1.1.0')
+            done()
+          })
+          resolve('some-package', '~2.0.0', function (err, pkg) {
+            assert.ifError(err)
+            assert.equal(pkg.version, '2.0.0')
+            done()
+          })
         })
 
         it('should properly cache requests', function () {
-          resolve('some-package', '~1.1.0', cb)
-
-          httpResp.statusCode = 200
-          http.get.args[0][1](httpResp)
-
-          httpResp.emit('data', JSON.stringify({
+          server.get('/some-package').reply(200, {
             versions: {
-              '0.0.1': { version: '0.0.1' },
-              '1.0.0': { version: '1.0.0' },
+              '0.0.1': { version: '0.0.1', dist: {} },
+              '1.0.0': { version: '1.0.0', dist: {} },
               '1.1.0': { version: '1.1.0', dist: {} },
               '2.0.0': { version: '2.0.0', dist: {} }
             }
-          }))
-          httpResp.emit('end')
-
-          assert.ifError(cb.args[0][0])
-          assert.deepEqual(cb.args[0][1].version, '1.1.0')
-
-          var cb2 = mock()
-          resolve('some-package', '~2.0.0', cb2)
-
-          assert.ifError(cb2.args[0][0])
-          assert.equal(cb2.args[0][1].version, '2.0.0')
-
-          assert.equal(cb.called, 1)
-          assert.equal(cb2.called, 1)
+          })
+          resolve('some-package', '~1.1.0', function (err, pkg) {
+            assert.ifError(err)
+            assert.equal(pkg.version, '1.1.0')
+            resolve('some-package', '~2.0.0', function (err, pkg) {
+              assert.ifError(err)
+              assert.equal(pkg.version, '2.0.0')
+            })
+          })
         })
       })
     })
