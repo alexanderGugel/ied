778b7f2f45c8092ca28492b7152b0230fde76ec4
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-22 19:57:37 +0800

    Add integration tests for scoped packages and semver ranges

--- test/integration/install.js
+++ test/integration/install.js
@@ -17,7 +17,9 @@ describe('install', function () {
     { name: 'mocha', version: '2.3.4', checkCli: true },
     { name: 'lodash', version: '3.10.1', checkCli: false },
     { name: 'gulp', version: 'https://github.com/gulpjs/gulp/archive/4.0.tar.gz', checkCli: true },
-    { name: 'grunt', version: '0.3.1', checkCli: true }
+    { name: 'grunt', version: '0.3.1', checkCli: true },
+    { name: '@rstacruz/tap-spec', version: '4.1.1', checkCli: false },
+    { name: 'rimraf', version: '^2', checkCli: false }
   ]
 
   function reset (done) {
