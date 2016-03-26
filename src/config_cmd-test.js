c63b376bd19285f2a1ab1008539bba10117eba71
Alexander Gugel <alexander.gugel@gmail.com>
2016-03-26 14:57:03 +0100

    config: Add tests

--- src/config_cmd-test.js
+++ src/config_cmd-test.js
@@ -11,8 +11,9 @@ afterEach(() => sandbox.restore())
 
 describe('configCmd', () => {
   it('should print all config variables', () => {
-    sandbox.spy(console, 'log')
+    sandbox.stub(console, 'log')
     configCmd()
+
     for (let key in config) {
       sinon.assert.calledWith(console.log, sinon.match(key))
       sinon.assert.calledWith(console.log, sinon.match(String(config[key])))
