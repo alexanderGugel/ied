81888a12b6f8deadc51388b14b977a048790e62f
Alexander Gugel <alexander.gugel@gmail.com>
2016-05-28 20:14:28 +0100

    Rewrite run command using RxJS

--- src/run_cmd.js
+++ src/run_cmd.js
@@ -1,5 +1,12 @@
 import path from 'path'
-import async from 'async'
+import assert from 'assert'
+import {readFile, entries} from './util'
+import {concatMap} from 'rxjs/operator/concatMap'
+import {Observable} from 'rxjs'
+import {map} from 'rxjs/operator/map'
+import {filter} from 'rxjs/operator/filter'
+import {reduce} from 'rxjs/operator/reduce'
+import {_do} from 'rxjs/operator/do'
 import {spawn} from 'child_process'
 import {sh, shFlag} from './config'
 
@@ -7,55 +14,58 @@ import {sh, shFlag} from './config'
  * run a `package.json` script and the related pre- and postscript.
  * @param {String} cwd - current working directory.
  * @param  {Object} argv - command line arguments.
+ * @return {Observable} - observable sequence.
  */
 export default function runCmd (cwd, argv) {
 	const scripts = argv._.slice(1)
-	const pkg = require(path.join(cwd, 'package.json'))
 
+	const pkgJson = readFile(path.join(cwd, 'package.json'), 'utf8')
+		::map(JSON.parse)
+
+	// if no script(s) have been specified, log out the available scripts.
 	if (!scripts.length) {
-		const availableScripts = Object.keys(pkg.scripts || [])
-		console.log('Available scripts: ' + availableScripts.join(', '))
-		return
+		return pkgJson::_do(logAvailable)
 	}
 
-	const env = Object.create(process.env)
-	env.PATH = [
-		path.join(cwd, 'node_modules/.bin'), process.env.PATH
-	].join(path.delimiter)
+	const PATH = [path.join(cwd, 'node_modules/.bin'), process.env.PATH]
+		.join(path.delimiter)
+	const env = {...process.env, PATH}
+	const runOptions = { env, stdio: 'inherit' }
 
-	async.mapSeries(scripts, (scriptName, cb) => {
-		const scripts = [
-			`pre${scriptName}`,
-			scriptName,
-			`post${scriptName}`
-		]
+	return pkgJson::map(({ scripts = {}}) => scripts)::entries()
+		::filter(([name]) => ~scripts.indexOf(name))
+		::concatMap(([name, script]) => run(script, runOptions)
+			::map((code) => ([name, script, code])))
+		::_do(logCode)
+		::reduce((codes, [name, script, code]) => codes + code, 0)
+		::_do((code) => assert.equal(code, 0, 'exit status != 0'))
+}
 
-		const scriptCmds = scripts.map((scriptName) => {
-			return pkg.scripts[scriptName]
-		}).filter((scriptCmd) => {
-			return scriptCmd
+export function run (script, options = {}) {
+	return Observable.create((observer) => {
+		const args = [shFlag, script]
+		const childProcess = spawn(sh, args, options)
+		childProcess.on('close', (code) => {
+			observer.next(code)
+			observer.complete()
 		})
-
-		if (!scriptCmds.length) return cb(null, null)
-
-		async.mapSeries(scriptCmds, (scriptCmd, cb) => {
-			const childProcess = spawn(sh, [shFlag, scriptCmd], {
-				env: env,
-				stdio: 'inherit'
-			})
-			childProcess.on('close', cb.bind(null, null))
-			childProcess.on('error', cb)
-		}, cb)
-	}, function (err, statuses) {
-		if (err) throw err
-		const info = scripts.map((script, i) => {
-			const scriptStatus = statuses[i].every((code) => { return code === 0 })
-			return `${script} exited with status ${scriptStatus ? 0 : 1}`
-		}).join('\n')
-		console.log(info)
-
-		const flattenedStatuses = [].concat.apply([], statuses)
-		const success = flattenedStatuses.every((code) => { return code === 0 })
-		process.exit(success ? 0 : 1)
+		childProcess.on('error', (err) => {
+			observer.error(err)
+		})
 	})
+}
+
+function logCode ([name, script, code]) {
+	const prefix = `${name}: \`${script}\``
+	if (code === 0) console.log(`${prefix} succeeded`)
+	else console.error(`${prefix} failed (exit status ${code})`)
+}
+
+function logAvailable ({ scripts = {}}) {
+	const available = Object.keys(scripts)
+	if (available.length) {
+		console.log(`available scripts: ${available.join(', ')}`)
+	} else {
+		console.log('no scripts in package.json')
+	}
 }
