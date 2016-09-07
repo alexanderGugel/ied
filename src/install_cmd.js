91867361de6cd6caa8c8653c646fc1d1774dfe50
Alexander Gugel <alexander.gugel@gmail.com>
2016-09-07 02:51:24 +0100

    WIP

--- src/install_cmd.js
+++ src/install_cmd.js
@@ -1,5 +1,6 @@
 import path from 'path'
 
+import {EmptyObservable} from 'rxjs/observable/EmptyObservable'
 import {concatStatic} from 'rxjs/operator/concat'
 import {map} from 'rxjs/operator/map'
 import {mergeStatic} from 'rxjs/operator/merge'
@@ -9,7 +10,7 @@ import {skip} from 'rxjs/operator/skip'
 import fetchAll from './fetch_all'
 import linkAll from './link_all'
 import resolveAll from './resolve_all'
-import {fromArgv, fromFs} from './pkg_json'
+import {fromArgv, fromFs, save} from './pkg_json'
 import {init as initCache} from './cache'
 
 function installAll (dir) {
@@ -24,6 +25,9 @@ const parseArgv = ({_, production}) => ({
 	isProd: production
 })
 
+const shouldSave = argv =>
+	!!(argv.save || argv['save-dev'] || argv['save-optional'])
+
 export default config => (cwd, argv) => {
 	const {isExplicit, isProd} = parseArgv(argv)
 	const dir = path.join(cwd, 'node_modules')
@@ -31,7 +35,13 @@ export default config => (cwd, argv) => {
 
 	// generate the "source" package.json file from which dependencies are being
 	// parsed and installed.
-	const srcPkgJson = isExplicit ? fromArgv(cwd, argv) : fromFs(cwd)
+	const srcPkgJson = isExplicit
+		? fromArgv(cwd, argv)
+		: fromFs(cwd)
+
+	const savedPkgJson = shouldSave(argv)
+		? srcPkgJson::save(cwd)
+		: EmptyObservable.create()
 
 	const installedAll = srcPkgJson
 		::map(pkgJson => ({
@@ -47,6 +57,7 @@ export default config => (cwd, argv) => {
 
 	return concatStatic(
 		initCache(),
-		installedAll
+		installedAll,
+		savedPkgJson
 	)
 }
