165d337f4f55c82655f1131b023143f0e9623255
Alexander Gugel <alexander.gugel@gmail.com>
2016-11-29 21:55:15 +0000

    Add MultiResolver to remove store logic

--- ied/resolver.go
+++ ied/resolver.go
@@ -7,3 +7,32 @@ type Resolver interface {
 	// be downloaded and linked.
 	Resolve(dir, name, version string) (Pkg, error)
 }
+
+// MultiResolver wraps multiple different resolvers, but is itself a resolver.
+type MultiResolver struct {
+	resolvers []Resolver
+}
+
+// NewMultiResolver wraps multiple resolvers and creates a new MultiResolver.
+func NewMultiResolver(resolvers ...Resolver) MultiResolver {
+	return MultiResolver{
+		resolvers: resolvers,
+	}
+}
+
+// Resolve resolves the specified dependency using all available resolvers. If
+// the dependency couldn't be resolved, nil will be returned as a package.
+func (r MultiResolver) Resolve(dir, name, version string) (Pkg, error) {
+	var pkg Pkg
+	for _, resolver := range r.resolvers {
+		var err error
+		pkg, err = resolver.Resolve(dir, name, version)
+		if err != nil {
+			return nil, err
+		}
+		if pkg != nil {
+			break
+		}
+	}
+	return pkg, nil
+}
