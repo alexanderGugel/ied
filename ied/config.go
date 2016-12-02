09da95ca9bb62c4ef3e700ae3b857419d56fdc63
Alexander Gugel <alexander.gugel@gmail.com>
2016-12-02 16:45:21 +0000

    Add config docs

--- ied/config.go
+++ ied/config.go
@@ -10,12 +10,15 @@ import (
 	"path/filepath"
 )
 
+// Config encapsulates global configuration options.
 type Config struct {
 	Registry  string `yaml:"Registry"`
 	LogLevel  string `yaml:"Log Level"`
 	LogFormat string `yaml:"Log Format"`
 }
 
+// NewDefaultConfig creates a new configuration, populated with sensible
+// defaults.
 func NewDefaultConfig() *Config {
 	return &Config{
 		Registry:  "https://registry.npmjs.com",
@@ -24,6 +27,8 @@ func NewDefaultConfig() *Config {
 	}
 }
 
+// configFilename returns the path from which the default ied config should be
+// read. By default this is the .ied.yaml file in the user's home directory.
 func configFilename() (string, error) {
 	usr, err := user.Current()
 	if err != nil {
@@ -32,6 +37,9 @@ func configFilename() (string, error) {
 	return filepath.Join(usr.HomeDir, ".ied.yaml"), nil
 }
 
+// LoadConfig reads the user's config from the provided filename. This function
+// always returns a config. User-defined configuration records override
+// predefined config defaults.
 func LoadConfig(filename string) (*Config, error) {
 	config := NewDefaultConfig()
 	raw, err := ioutil.ReadFile(filename)
@@ -45,6 +53,8 @@ func LoadConfig(filename string) (*Config, error) {
 	return config, nil
 }
 
+// Validate checks it the provided config is valid. It ensures that the provided
+// registry URL is syntactically valid and the log level exists.
 func (c *Config) Validate() error {
 	if c.Registry == "" {
 		return errors.New("missing registry")
