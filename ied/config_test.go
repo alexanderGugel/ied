9096fbfe2c086544af463230584b0fd18b618726
Alexander Gugel <alexander.gugel@gmail.com>
2016-12-02 13:43:35 +0000

    Test LoadConfig

--- ied/config_test.go
+++ ied/config_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -10,4 +13,25 @@ func TestNewDefaultConfig(t *testing.T) {
 	assert.Equal(t, config.Registry, "https://registry.npmjs.com")
 	assert.Equal(t, config.LogLevel, "info")
 	assert.Equal(t, config.LogFormat, "json")
+}
+
+func TestLoadConfig(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestLoadConfig")
+	assert.NoError(t, err)
+
+	defer os.RemoveAll(dir)
+
+	filename := filepath.Join(dir, "config.yaml")
+	raw := []byte(`Registry: https://registry.npmjs.com
+Log Level: warn`)
+	err = ioutil.WriteFile(filename, raw, os.ModePerm)
+	assert.NoError(t, err)
+
+	config, err := LoadConfig(filename)
+	assert.NoError(t, err)
+	assert.Equal(t, *config, Config{
+		Registry:  "https://registry.npmjs.com",
+		LogLevel:  "warn",
+		LogFormat: "json",
+	})
 }
